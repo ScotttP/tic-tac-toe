2d047646ab35591177cf14f66f278ccc863458dd
Scott Pribe <Scottpribe@gmail.com>
2020-05-07 21:11:31 -0400

    refactored the functions to make addFunctions available to the reset module

--- script.js
+++ script.js
@@ -32,85 +32,51 @@ const gameBoard = (() => {
         ['0','','','','4','','','','8'],['','','2','','4','','6','',''],
     ];
     
-    startButton.addEventListener('click', initialStart,false)
+
+    
+startButton.addEventListener('click', initialStart,false)
     
     function initialStart() {
         playerNames = Players(player1Name,player2Name);
         resetButton.addEventListener('click', reset, false)
-        
+
         xCount = 0;
         oCount = 0;
         
-        if (playerNames.player1Name === undefined || playerNames.player1Name === '' 
-        && playerNames.player2Name === undefined || playerNames.player2Name === ''){
+        startButton.disabled = true;
+        resetButton.disabled = false;
+
+        
+        if (playerNames.player1Name === '' || playerNames.player2Name === ''){
             alert("Please Enter Your Name.")
             return
         }else{
-                startButton.disabled = true;
-                resetButton.disabled = true;
-
-                currentPlayerTurn.innerText = `${playerNames.player1Name} TURN`
-
-                gridbuttons.forEach((div) => {//calls the functions when clicked on a square
-                    div.addEventListener('click', addFunctions,false)
-
-                    function addFunctions() { //adds the render,updategameboard functions and is called when clicked on
-                        let id = div.id;
-                        if (playerNames.player1Name === '' || playerNames.player2Name === ''){
-                            alert("Please Enter Your Name.")
-                            return
-                        }else{
-
-                            render(id,currentPlayerTurn,playerNames)//this renders on the board needs to be gamePlay.currentPlayerTurn because it's in the gamePlay module and not global
-                                
-                            if (document.getElementById(id).innerText === 'X'){ //if the grid at a certain id contains an X, increase the count of X.
-                                ++xCount
-                                console.log(document.getElementById(id).innerText)
-                                    
-                            }
-                            if (document.getElementById(id).innerText === 'O'){//if the grid at a certain id contains an O, increase the count of O.
-                                ++oCount
-                                console.log(document.getElementById(id).innerText)
-                            }
-
-                            updateGameboard(id,playerNames,div);
-                            console.log(`Current X Count: ${xCount}`)
-                            console.log(`Current O Count: ${oCount}`)
-                        
-                    }
-                    div.removeEventListener('click', addFunctions,false) 
-                    }
-                    
-                })
+        currentPlayerTurn.innerText = `${playerNames.player1Name} TURN`
+        gridbuttons.forEach(div => {
+            let id = div.id;
+            div.addEventListener('click', addFunctions(id),false)
+            })
+        }
         
-        }
-        //startButton.removeEventListener('click',initialStart,false) // removes the initital start fucntion
     }
-    
-    function updateGameboard (id,playerNames,div) {
-        currentGameBoard.splice(id,1,id);
-        if (xCount >= 3 || oCount >= 3){
-            gamePlay.compareWin(currentGameBoard,_winningCombinations,playerNames,xCount,oCount,div) //calls compare win function in the 
-        }
-    }
-    
-    function render (id,currentPlayerTurn,playerNames) {//toggle function between player turn
-       console.log(currentPlayerTurn.innerText)
-       
-            if (currentPlayerTurn.innerText === `${playerNames.player1Name} TURN` || 
-            currentPlayerTurn.innerText === '' ){
-                document.getElementById(id).innerText = 'X';
-                currentPlayerTurn.innerText = `${playerNames.player2Name} TURN`;
+      
+       function addFunctions(id) { //adds the render,updategameboard functions and is called when clicked on
+        render(id,currentPlayerTurn,playerNames)//this renders on the board needs to be gamePlay.currentPlayerTurn because it's in the gamePlay module and not global
+            
+        if (document.getElementById(id).innerText === 'X'){ //if the grid at a certain id contains an X, increase the count of X.
+            ++xCount
+            console.log(document.getElementById(id).innerText)
                 
-            }else if (currentPlayerTurn.innerText === `${playerNames.player2Name} TURN`){
-                document.getElementById(id).innerText = 'O';
-                currentPlayerTurn.innerText =`${playerNames.player1Name} TURN`;
-            }else{
-               
-            }
-       
+        }
+        if (document.getElementById(id).innerText === 'O'){//if the grid at a certain id contains an O, increase the count of O.
+            ++oCount
+            console.log(document.getElementById(id).innerText)
+        }
 
-    }
+        updateGameboard(id,playerNames);
+        console.log(`Current X Count: ${xCount}`)
+        console.log(`Current O Count: ${oCount}`)
+        }
 
     function reset () {
         playerNames = Players(player1Name,player2Name);//makes the player names available
@@ -133,8 +99,34 @@ const gameBoard = (() => {
         resetButton.disabled = true;
         //resetButton.removeEventListener('click', reset, false) 
         startButton.disabled = false;
-        //startButton.addEventListener('click', initialStart, false) // adds the start listener back since the game ended.
+        startButton.addEventListener('click', initialStart, false) // adds the start listener back since the game ended.
+        gridbuttons.forEach(div => {
+            div.removeEventListener('click', addFunctions,false) 
+        })
+        
     }
+
+    
+    function updateGameboard (id,playerNames) {
+        currentGameBoard.splice(id,1,id);
+        gamePlay.compareWin(currentGameBoard,_winningCombinations,playerNames,xCount,oCount) //calls compare win function in the 
+        
+    }
+    
+    function render (id,currentPlayerTurn,playerNames) {//toggle function between player turn
+
+        if (currentPlayerTurn.innerText === `${playerNames.player1Name} TURN` || 
+        currentPlayerTurn.innerText === '' ){
+            document.getElementById(id).innerText = 'X';
+            currentPlayerTurn.innerText = `${playerNames.player2Name} TURN`;
+        }else if (currentPlayerTurn.innerText === `${playerNames.player2Name} TURN`){
+            document.getElementById(id).innerText = 'O';
+            currentPlayerTurn.innerText =`${playerNames.player1Name} TURN`;
+        }
+
+    }
+
+
     
     return {
         currentPlayerTurn,
@@ -145,10 +137,11 @@ const gameBoard = (() => {
         resetButton}
 })();
 
+
 const gamePlay = (() => {
     'use strict';
 
-    function compareWin (currentGameBoard,_winningCombinations,playerNames,xCount,oCount,div) {
+    function compareWin (currentGameBoard,_winningCombinations,playerNames,xCount,oCount) {
         if (currentGameBoard.includes(_winningCombinations[0][0,1,2]) 
         || currentGameBoard.includes(_winningCombinations[1][3,4,5]) 
         || currentGameBoard.includes(_winningCombinations[2][6,7,8]) 
@@ -157,10 +150,10 @@ const gamePlay = (() => {
         || currentGameBoard.includes(_winningCombinations[5][2,5,8]) 
         || currentGameBoard.includes(_winningCombinations[6][0,4,8])
         || currentGameBoard.includes(_winningCombinations[7][2,4,6])){
-            checkSign(playerNames,xCount,oCount,div);
+            checkSign(playerNames,xCount,oCount);
             }
     }
-    function checkSign(playerNames,xCount,oCount,div) { 
+    function checkSign(playerNames,xCount,oCount) { 
         if(document.getElementById(0).innerText === 'X' && document.getElementById(1).innerText === 'X' && document.getElementById(2).innerText === 'X'
         || document.getElementById(3).innerText === 'X' && document.getElementById(4).innerText === 'X' && document.getElementById(5).innerText === 'X'
         || document.getElementById(6).innerText === 'X' && document.getElementById(7).innerText === 'X' && document.getElementById(8).innerText === 'X'
@@ -191,7 +184,7 @@ const gamePlay = (() => {
             startButton.disabled = false;
            
 
-        }else if (xCount === 5 && oCount === 4 || xCount === 4 && oCount === 5){
+        }else if(xCount === 5 && oCount === 4 || xCount === 4 && oCount === 5){
             gameBoard.currentPlayerTurn.innerText = "TIE!"
             xCount = 0;
             oCount = 0;
@@ -204,10 +197,4 @@ const gamePlay = (() => {
     
     return{compareWin,checkSign}
 })();
-
-
-
-
-
-
 
